25fbbc30e053301e62b57202d7f9cf8e4822b495
M0kht4r <117685106+dev-Mokhtar@users.noreply.github.com>
2023-04-02 01:02:53 +0500

    Create Sponsors.js

---
+++ src/components/sponsors/Sponsors.js
@@ -0,0 +1,141 @@
+Sponsors function (){
+
+
+  return(<div>
+    <link href="sponsors.css" rel="stylesheet" />
+    <div>
+      <div class="sponsors-sponsors">
+        <img
+          src="Vector 2.svg"
+          alt="Vector"
+          class="sponsors-vector2"
+        />
+        <div class="sponsors-group7">
+          <img
+            src="Rectangle 17.png"
+            alt="ooredoo"
+            class="sponsors-rectangle17"
+          />
+        </div>
+        <div class="sponsors-group8">
+          <img
+            src="Rectangle 18dd.png"
+            alt="proxylan"
+            class="sponsors-rectangle18"
+          />
+        </div>
+        <div class="sponsors-group9">
+          <img
+            src="Rectangle 19.png"
+            alt="nothing here"
+            class="sponsors-rectangle19"
+          />
+        </div>
+        <span class="sponsors-text">Thank you for sponsoring us</span>
+        <span class="sponsors-text01"><span>They trusted us</span></span>
+        <div class="sponsors-frame14">
+          <div class="sponsors-group1">
+            <img
+              src="Line 5.png"
+              alt="Line5117"
+              class="sponsors-line5"
+            />
+            <span class="sponsors-text03"><span>Get in touch with us</span></span>
+            <img
+              src="/playground_assets/line6119-b1c.svg"
+              alt="Line6119"
+              class="sponsors-line6"
+            />
+          </div>
+          <div class="sponsors-rights">
+            <span class="sponsors-text05"><span>All Rights Reserved</span></span>
+            <span class="sponsors-text07"><span>Code &amp; Share 2023</span></span>
+            <div class="sponsors-maskgroup">
+              <img
+                src="Mask group.png"
+                alt="Copyright124"
+                class="sponsors-copyright"
+              />
+
+            </div>
+            <img
+              src="/playground_assets/line3126-cua.svg"
+              alt="Line3126"
+              class="sponsors-line3"
+            />
+          </div>
+          <div class="sponsors-contact">
+            <div class="sponsors-group6">
+              <div class="sponsors-socialicons">
+                <div class="sponsors-insta">
+                  <img
+                    src="Instagram Circle.png"
+                    alt="InstagramCircle131"
+                    class="sponsors-instagram-circle"
+                  />
+                  <img
+                    src="Insta.png"
+                    alt="insta"
+                    class="sponsors-rectangle10"
+                  />
+                </div>
+                <div class="sponsors-fb">
+                  <img
+                    src="Fb.png"
+                    alt="Facebook134"
+                    class="sponsors-facebook"
+                  />
+
+
+                </div>
+                <div class="sponsors-twitter">
+                  <img
+                    src="twitter.png"
+                    alt="Twitter137"
+                    class="sponsors-twitter1"
+                  />
+                </div>
+                <div class="sponsors-pinterest">
+                  <img
+                    src="pinterest.png"
+                    alt="Pinterest140"
+                    class="sponsors-pinterest1"
+                  />
+
+
+                </div>
+              </div>
+              <span class="sponsors-text09"><span>Follow Us</span></span>
+            </div>
+            <div class="sponsors-group5">
+              <span class="sponsors-text11"><span>About Us</span></span>
+              <span class="sponsors-text13">
+                <span>
+                  <span>About C&amp;S</span>
+                  <br />
+                  <span></span>
+                </span>
+              </span>
+              <span class="sponsors-text18"><span>Achievements</span></span>
+            </div>
+            <div class="sponsors-group4">
+              <span class="sponsors-text20"><span>Our Events</span></span>
+              <span class="sponsors-text22"><span>Coffee Outlet</span></span>
+              <span class="sponsors-text24"><span>StarterPack</span></span>
+              <span class="sponsors-text26"><span>Contact Us</span></span>
+            </div>
+            <div class="sponsors-group3">
+              <span class="sponsors-text28"><span>Ressources</span></span>
+              <span class="sponsors-text30"><span>FAQ</span></span>
+              <span class="sponsors-text32"><span>Partners</span></span>
+              <span class="sponsors-text34"><span>Support us</span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>)
+
+}
+
+export default Sponsors
