d353cc279c7199e997bfbfd797cd4fe7aa613eaf
M0kht4r <117685106+dev-Mokhtar@users.noreply.github.com>
2023-04-03 14:06:02 +0100

    Update Sponsors.jsx

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 
 
 function Sponsors() {
-    //...
-    return (
-        <div className="font-sans">Sponsors</div>
-    );
-}
+  const [sponsors, setSponsors] = useState([]);
 
+  useEffect(() => {
+    fetch('https://codeandshare.pythonanywhere.com/sponsors/')
+      .then((response) => response.json())
+      .then((data) => setSponsors(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="sponsors">
+      <h2>Sponsors</h2>
+      <div className="sponsors-list">
+        {sponsors.map((sponsor) => (
+          <div key={sponsor.id} className="sponsor">
+            <img src={sponsor.icon} alt={sponsor.name} />
+            <p>{sponsor.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 export default Sponsors;
